c2b1bfd00daaf03fcde948d0ab723f34a10fd276
Carly Hayter <carlyhayter@gmail.com>
2021-12-22 13:15:45 -0800

    udpated google analytics to use next's tech

--- pages/_app.js
+++ pages/_app.js
@@ -7,19 +7,38 @@ import "../styles/homePage.css";
 import "../styles/galleryPage.css";
 import "../styles/fineart.css";
 import "../styles/web.css";
-import ReactGA from "react-ga";
+import { Router, useRouter } from "next/router";
+import { initGA, logPageView } from "../utils/analytics";
 
 import "font-awesome/css/font-awesome.css";
-
 import NavBar from "../components/NavBar";
 
-ReactGA.initialize("UA-100854465-2");
-
 function MyApp({ Component, pageProps }) {
-  ReactGA.pageview(window.location.pathname + window.location.search);
+  const myRouter = useRouter();
+
+  useEffect(() => {
+    initGA();
+    // `routeChangeComplete` won't run for the first page load unless the query string is
+    // hydrated later on, so here we log a page view if this is the first render and
+    // there's no query string
+    if (!myRouter.asPath.includes("?")) {
+      logPageView();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    // Listen for page changes after a navigation or when the query changes
+    myRouter.events.on("routeChangeComplete", logPageView);
+    return () => {
+      myRouter.events.off("routeChangeComplete", logPageView);
+    };
+  }, [myRouter.events]);
+
   return (
     <>
       <NavBar />
+
       <Component {...pageProps} />
     </>
   );
