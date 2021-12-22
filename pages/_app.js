80df7b8cd02e1700f1ddc278d86d48c6b9774fa6
Carly Hayter <carlyhayter@gmail.com>
2021-12-22 13:00:00 -0800

    added google analtics tracking

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,16 @@ import "../styles/homePage.css";
 import "../styles/galleryPage.css";
 import "../styles/fineart.css";
 import "../styles/web.css";
+import ReactGA from "react-ga";
 
 import "font-awesome/css/font-awesome.css";
 
 import NavBar from "../components/NavBar";
 
+ReactGA.initialize("UA-100854465-2");
+
 function MyApp({ Component, pageProps }) {
+  ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <>
       <NavBar />
