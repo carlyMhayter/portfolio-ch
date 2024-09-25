d721203f67cbc74b3eef3b7862ed0308715ead3e
carlyMhayter <carlyhayter@gmail.com>
2024-09-25 11:37:23 -0700

    add trapezoid to dropdown

--- components/FloatingMenu.js
+++ components/FloatingMenu.js
@@ -50,37 +50,39 @@ const MenuLink = styled.li`
   }
 `;
 
-const DropdownContainer = styled.h1`
+const DropdownContainer = styled.div`
   position: absolute;
   height: fit-content;
-  padding-top: 2rem;
+  padding-top: 3rem;
+  padding-left: 1rem;
+  padding-right: 0.5rem;
+  border-radius: 10px;
   width: fit-content;
   z-index: 40;
   right: 0;
   top: ${(props) => (props.show ? `-2rem` : ` -10rem`)};
   transition: 0.75s all ease;
   background: transparent;
-  border-bottom: 7px solid ${(props) => ` ${props.color}`};
   font-weight: 800;
   font-size: 40px;
   text-transform: lowercase;
   font-style: italic;
-  color: black;
   ${(props) => `color:  ${props.color};`}
+  background-color: pink;
+  ${(props) => `background-color:  ${props.color};`}
 
+  transform: perspective(30px) rotateX(0deg) rotateY(2deg);
   @media only screen and (min-width: 480px) {
     font-size: 50px;
   }
   @media only screen and (min-width: 640px) {
     font-size: 60px;
-    border-bottom: 8px solid ${(props) => ` ${props.color}`};
   }
   @media only screen and (min-width: 768px) {
     font-size: 70px;
   }
   @media only screen and (min-width: 1024px) {
     font-size: 80px;
-    border-bottom: 9px solid ${(props) => ` ${props.color}`};
   }
   @media only screen and (min-width: 1280px) {
     font-size: 90px;
@@ -91,7 +93,48 @@ const DropdownContainer = styled.h1`
 
   @media only screen and (min-width: 2000px) {
     font-size: 100px;
-    border-bottom: 10px solid ${(props) => ` ${props.color}`};
+  }
+`;
+
+const DropdownText = styled.h1`
+  position: absolute;
+  z-index: 40;
+  right: 0em;
+  /* border: 1px solid red; */
+  top: ${(props) => (props.show ? `0rem` : ` -10rem`)};
+  transition: 1s all ease;
+  background: transparent;
+  font-weight: 800;
+  font-size: 40px;
+  text-transform: lowercase;
+  font-style: italic;
+  color: var(--vvLtGrey);
+  /* ${(props) => `color:  ${props.color};`} */
+  padding-right: 0.5em;
+  @media only screen and (min-width: 480px) {
+    font-size: 50px;
+  }
+  @media only screen and (min-width: 640px) {
+    font-size: 60px;
+    /* border-bottom: 8px solid ${(props) => ` ${props.color}`}; */
+  }
+  @media only screen and (min-width: 768px) {
+    font-size: 70px;
+  }
+  @media only screen and (min-width: 1024px) {
+    font-size: 80px;
+    /* border-bottom: 9px solid ${(props) => ` ${props.color}`}; */
+  }
+  @media only screen and (min-width: 1280px) {
+    font-size: 90px;
+  }
+  @media only screen and (min-width: 1536px) {
+    font-size: 95px;
+  }
+
+  @media only screen and (min-width: 2000px) {
+    font-size: 100px;
+    /* border-bottom: 10px solid ${(props) => ` ${props.color}`}; */
   }
 `;
 
@@ -137,6 +180,11 @@ function FloatingMenu() {
         <DropdownContainer color={current.color} show={show}>
           {current.name}
         </DropdownContainer>
+      )}
+      {current.name !== 'Home' && (
+        <DropdownText color={current.color} show={show}>
+          {current.name}
+        </DropdownText>
       )}
 
       <OuterMenuContainer>
